97c4da06761f14479ca40345707ae8601ec8cc3f
achinter216 <achintar216@gmail.com>
2024-09-12 16:50:01 +0530

    update

--- js/novel.js
+++ js/novel.js
@@ -1,3 +1,19 @@
+document.querySelectorAll('.menu-btn').forEach(button => {
+  button.addEventListener('click', () => {
+    const submenu = button.nextElementSibling;
+
+    if (submenu.style.display === "block") {
+      submenu.style.display = "none";
+    } else {
+      submenu.style.display = "block";
+    }
+  });
+});
+
+
+
+
+
 function fileExists(url)
     {
     var http = new XMLHttpRequest();
@@ -5,3 +21,5 @@ function fileExists(url)
     http.send();
     return http.status!=404;
     }
+    
+    
