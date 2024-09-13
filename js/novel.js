c3224ea1de9fc02eac21dcfeebc37568aef66789
noobsuper2648 <dhorbendroroy@gmail.com>
2024-09-13 13:23:40 +0530

    update

--- js/novel.js
+++ js/novel.js
@@ -10,3 +10,30 @@ document.querySelectorAll('.menu-btn').forEach(button => {
     }
   });
 });
+
+//this is a gpt code //
+var modal = document.getElementById("myModal");
+
+
+var img = document.getElementById("demoimg");
+
+
+var closeBtn = document.getElementsByClassName("close")[0];
+
+
+img.onclick = function() {
+    modal.style.display = "block";
+}
+
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+//this is a gpt code ^^//
