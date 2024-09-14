3228e82b123942d348268bcb895580b685c1cf16
achinter216 <achintar216@gmail.com>
2024-09-14 06:19:40 +0530

    update

---
+++ js/youtubev.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.menu-btn').forEach(button => {
+  button.addEventListener('click', () => {
+    const submenu = button.nextElementSibling;
+
+    if (submenu.style.display === "block") {
+      submenu.style.display = "none";
+    } else {
+      submenu.style.display = "block";
+    }
+  });
+});
